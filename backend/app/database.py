0f366101db6f364b5a98630c14738359d915a54e
shaheerkhan <its.shaheerkhan728@gmail.com>
2026-05-03 13:45:53 +0530

    connected render postgres

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-DATABASE_URL = "postgresql://postgres:12345@localhost/document_system"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
